0a9ccb4621c0715fd48d1bce5205012519376f16
Jatin Sandilya <sandilya.jatin@gmail.com>
2024-07-26 10:52:09 +0530

    feat: add hubspot ui & amp config

---
+++ client/src/App.tsx
@@ -0,0 +1,54 @@
+import { AmpersandProvider, InstallIntegration } from "@amp-labs/react";
+
+function App() {
+  const options = {
+    projectId: import.meta.env.VITE_AMPERSAND_PROJECT_ID, // Your Ampersand project ID.
+    apiKey: import.meta.env.VITE_AMPERSAND_API_KEY, // Your Ampersand API key.
+  };
+
+  const myIntegrationName = "readListsAndContacts"
+  const userId = "demoUserId"
+  const userFullName = "Demo User"
+  const teamId = "demoTeamId"
+  const teamName = "Demo Team Name"
+
+  return (
+    <AmpersandProvider options={options}>
+      <div
+        style={{
+          margin: "2rem",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <InstallIntegration
+          integration={myIntegrationName}
+          consumerRef={userId}
+          consumerName={userFullName}
+          groupRef={teamId}
+          groupName={teamName}
+          onInstallSuccess={(installationId, configObject) =>
+            console.log(
+              `Successfully installed ${installationId} with configuration ${JSON.stringify(
+                configObject,
+                null,
+                2
+              )}`
+            )
+          }
+          onUpdateSuccess={(installationId, configObject) =>
+            console.log(
+              `Successfully updated ${installationId} with configuration ${JSON.stringify(
+                configObject,
+                null,
+                2
+              )}`
+            )
+          }
+        />
+      </div>
+    </AmpersandProvider>
+  );
+}
+
+export default App;
