76838c25cbb47e6b0a989b28de899dc5a8fb686c
Jatin Sandilya <sandilya.jatin@gmail.com>
2024-08-01 13:55:34 +0530

    [ENG-1324] fix: ensure apollo integration works from frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,23 +6,51 @@ function App() {
     apiKey: import.meta.env.VITE_AMPERSAND_API_KEY as string, // Your Ampersand API key.
   };
 
-  const myIntegrationName = "readListsAndContacts"
-  const userId = "demoUserId"
-  const userFullName = "Demo User"
-  const teamId = "demoTeamId"
-  const teamName = "Demo Team Name"
+  const myIntegrationName = "readHubspotListsAndContacts";
+  const apolloIntegrationName = "readApolloContacts";
+  const userId = "demoUserId";
+  const userFullName = "Demo User";
+  const teamId = "demoTeamId";
+  const teamName = "Demo Team Name";
 
   return (
     <AmpersandProvider options={options}>
       <div
         style={{
-          margin: "2rem",
+          margin: "5rem",
           display: "flex",
           justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
         }}
       >
         <InstallIntegration
           integration={myIntegrationName}
+          consumerRef={userId}
+          consumerName={userFullName}
+          groupRef={teamId}
+          groupName={teamName}
+          onInstallSuccess={(installationId, configObject) =>
+            console.log(
+              `Successfully installed ${installationId} with configuration ${JSON.stringify(
+                configObject,
+                null,
+                2
+              )}`
+            )
+          }
+          onUpdateSuccess={(installationId, configObject) =>
+            console.log(
+              `Successfully updated ${installationId} with configuration ${JSON.stringify(
+                configObject,
+                null,
+                2 
+              )}`
+            )
+          }
+        />
+        <InstallIntegration
+          integration={apolloIntegrationName}
           consumerRef={userId}
           consumerName={userFullName}
           groupRef={teamId}
