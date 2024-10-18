f188b091e4141f34b2ee4a21b40e2f11e1a27d18
Jatin Sandilya <sandilya.jatin@gmail.com>
2024-10-18 19:26:04 +0530

    update installintegration to include dynamic mappings

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,15 +6,14 @@ function App() {
     apiKey: import.meta.env.VITE_AMPERSAND_API_KEY as string, // Your Ampersand API key.
   };
 
-  const myIntegrationName = "readHubspotListsAndContacts";
-  const apolloIntegrationName = "readApolloContacts";
+  const myIntegrationName = "readHubspotContactsWithCustomFields";
   const userId = "demoUserId";
   const userFullName = "Demo User";
-  const teamId = "ampersand-hubspot-demo-team-id";
+  const teamId = "ampersand-hubspot-demo-team-id-3";
   const teamName = "Demo Team Name";
 
   return (
-    <AmpersandProvider options={options}>
+    <AmpersandProvider options={options}> 
       <div
         style={{
           margin: "15rem",
@@ -31,6 +30,27 @@ function App() {
             consumerName={userFullName}
             groupRef={teamId}
             groupName={teamName}
+            fieldMapping={{
+              contacts: [
+                {
+                  mapToName: 'source',
+                  mapToDisplayName: 'Source',
+                  prompt: 'Which field do you use to track the source of a contact?',
+                },
+                {
+                  mapToName: 'pronoun',
+                  mapToDisplayName: 'Pronoun(2)',
+                  prompt: 'WHICH field do you use to track the pronouns of a contact?',
+                }
+              ],
+              companies: [
+                {
+                  mapToName: 'territory',
+                  mapToDisplayName: 'Sales Territory',
+                  prompt: 'Which field do you use to track the sales territory of a company?',
+                }
+              ]
+            }}
             onInstallSuccess={(installationId, configObject) =>
               console.log(
                 `Successfully installed ${installationId} with configuration ${JSON.stringify(
