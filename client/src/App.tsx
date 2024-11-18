ad6259595a0b86b8ddd4bfa2686511d0b070955d
Jatin Sandilya <sandilya.jatin@gmail.com>
2024-11-18 13:12:24 +0530

    update demo to do crsf testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,16 @@
 import { AmpersandProvider, InstallIntegration } from "@amp-labs/react";
 
+
 function App() {
   const options = {
     projectId: import.meta.env.VITE_AMPERSAND_PROJECT_ID as string, // Your Ampersand project ID.
     apiKey: import.meta.env.VITE_AMPERSAND_API_KEY as string, // Your Ampersand API key.
   };
 
-  const myIntegrationName = "readHubspotListsAndContacts";
-  const apolloIntegrationName = "readApolloContacts";
+  const myIntegrationName = "readSalesforceContacts";
   const userId = "demoUserId";
   const userFullName = "Demo User";
-  const teamId = "ampersand-hubspot-demo-team-id";
+  const teamId = "ampersand-hubspot-demo-team-id-12345";
   const teamName = "Demo Team Name";
 
   return (
@@ -27,33 +27,6 @@ function App() {
         <div style={{ display: "flex", marginTop:40 }}>
           <InstallIntegration
             integration={myIntegrationName}
-            consumerRef={userId}
-            consumerName={userFullName}
-            groupRef={teamId}
-            groupName={teamName}
-            onInstallSuccess={(installationId, configObject) =>
-              console.log(
-                `Successfully installed ${installationId} with configuration ${JSON.stringify(
-                  configObject,
-                  null,
-                  2
-                )}`
-              )
-            }
-            onUpdateSuccess={(installationId, configObject) =>
-              console.log(
-                `Successfully updated ${installationId} with configuration ${JSON.stringify(
-                  configObject,
-                  null,
-                  2
-                )}`
-              )
-            }
-          />
-        </div>
-        <div style={{ display: "flex" }}>
-          <InstallIntegration
-            integration={apolloIntegrationName}
             consumerRef={userId}
             consumerName={userFullName}
             groupRef={teamId}
